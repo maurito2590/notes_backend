25721ed577c5444aac74b0735be79683a0eb0a45
MaurO <maurito2590@gmail.com>
2026-02-21 19:09:40 -0300

    hope render running

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
